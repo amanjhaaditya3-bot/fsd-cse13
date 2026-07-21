e04a46b3ab98e76fec902c850e3b935f7469ca48
shiv <shivbhardwaj1197@gmail.com>
2026-07-21 14:47:54 +0530

    add function

---
+++ lab1/hello.js
@@ -0,0 +1,16 @@
+function sayHello(name) {
+  console.log(`Welcome to ${name}`);
+}
+function sum(a, b) {
+  return a + b;
+}
+
+sayHello("Aditya Aman Jha");
+console.log(`sum of 2 and 5 is ${sum(2, 5)}`);
+
+const sayHi = (name) => {
+  console.log(`Welcome to ${name}`);
+};
+sayHi("Aditya Aman Jha");
+const findsum = (a, b) => a + b;
+console.log(`sum of 2 and 5 is ${findsum(2, 5)}`);
