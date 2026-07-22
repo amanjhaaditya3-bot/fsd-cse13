e43d2c48964d851d02179d318c4fc36be53ad9e2
shiv <shivbhardwaj1197@gmail.com>
2026-07-22 11:14:43 +0530

    number word

---
+++ lab1/numbers.js
@@ -0,0 +1,25 @@
+// write a function to create number from 0 to 9 and print in words.
+const numbr = (a) => {
+    if (a==0)
+        console.log('zero');
+    else if (a==1)
+        console.log('one');
+     else if (a==2)
+        console.log('two'); 
+    else if (a==3)
+        console.log('three');
+    else if (a==4)
+        console.log('four');
+else if (a==5)
+        console.log('five');
+else if (a==6)
+        console.log('six');
+else if (a==7)
+        console.log('seven');
+    else if (a==8)
+        console.log('eight');
+else if (a==9)
+        console.log('nine');
+
+}
+numbr(8);
